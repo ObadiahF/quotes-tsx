db265e9c6ca4be44dafe94aff8c08e42a86fc5c8
Obadiah F <106855190+ObadiahF@users.noreply.github.com>
2023-09-13 00:18:01 -0700

    Added styling and made next button work

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,47 @@
-
 import './App.css'
+import { getNewQuote } from './api/quotesApi'
+import { useEffect, useState } from 'react'
+
 
 function App() {
+  
+
+  const [currentQuote, setNewQuote] = useState(
+    [
+    {
+      "quote": "",
+      "author": "",
+      "category": ""
+    }
+  ]
+  );
+
+  const getNextQuote = async () => {
+    const newQuote = await getNewQuote();
+    if (!newQuote) return
+
+    setNewQuote(newQuote);
+  }
+
+  useEffect(() => {
+    getNextQuote();
+  }, []);
+
   return (
     <div className="container">
+      <div className="title-container">
+        <h1>Quotes <span>TSX</span></h1>
+      </div>
+
+        <div className="quotes-container">
+          <h3>{currentQuote[0].quote}</h3>
+          <h4>{currentQuote[0].author}</h4>
+        </div>
+
+        <div className="btns-container">
+          <button>Save</button>
+          <button onClick={getNextQuote}>Next</button>
+        </div>
     </div>
   )
 }
---
+++ src/api/quotesApi.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+//import 'dotenv/config'
+
+const apiKey = 'YOUR API KEY HERE'
+//process.env.apiKey;
+
+export const getNewQuote = async () => {
+    try {
+        const response = await axios.get('https://api.api-ninjas.com/v1/quotes?category=', {
+            headers: { 'X-Api-Key': apiKey },
+        });
+        return response.data;
+    } catch (err) {
+        console.error(err);
+    }
+}
