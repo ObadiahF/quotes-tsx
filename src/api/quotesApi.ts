dd93020138c8cfaea9bcf92b7d71f03f4f07606e
ObadiahF <106855190+ObadiahF@users.noreply.github.com>
2024-03-16 12:18:41 -0700

    Fixed styling issues and added node server for api requests

--- src/api/quotesApi.ts
+++ src/api/quotesApi.ts
@@ -1,19 +1,9 @@
 import axios from "axios";
 
-// your api ninjas API Key here
-const apiKey = ''
-
 
 export const getNewQuote = async () => {
-    if (!apiKey || apiKey === "") {
-        throw Error('No Api Key Found')
-    }
-
     try {
-        
-        const response = await axios.get('https://api.api-ninjas.com/v1/quotes?category=', {
-            headers: { 'X-Api-Key': apiKey },
-        });
+        const response = await axios.get('http://localhost:3000/quotes');
         return response.data;
     } catch (err) {
         console.error(err);
